eac5f7c0ccbfcfaf92adfc9ef007e4c4cfe60aea
simantini bhattacharya <simantinibhattacharya@simantinis-air.lan>
2021-02-08 07:56:04 -0800

    Interchange the first and second fragment

--- app/src/main/java/com/example/gmgithubconnect/SecondFragment.java
+++ app/src/main/java/com/example/gmgithubconnect/SecondFragment.java
@@ -1,15 +1,19 @@
 package com.example.gmgithubconnect;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
+    SharedPreferences pref;
+    EditText enter_text;
 
     @Override
     public View onCreateView(
@@ -17,7 +21,9 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view =  inflater.inflate(R.layout.fragment_second, container, false);
+        enter_text = view.findViewById(R.id.textview_second);
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -26,9 +32,12 @@ public class SecondFragment extends Fragment {
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Save the user_name first
+                
                 NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                        .navigate(R.id.action_firstScreen);
             }
         });
+        //action_secondScreen
     }
 }
